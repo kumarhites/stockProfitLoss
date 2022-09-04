4a6337d09ff423ef43f998f6c99cc6e057aedd2b
Hitesh Kumar <hkhiteshkumar66@gmail.com>
2022-09-04 15:24:45 +0530

    fixed floating point and update ui

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const initialPrice = document.querySelector("#priceInput");
 const qunatity = document.querySelector("#qtyInput");
 const currentPrice = document.querySelector("#currentInput");
 const outputText = document.querySelector("#outputText")
-const errText = document.querySelector("#errText")
+// const outputText = document.querySelector("#outputText")
 const submitBtn = document.querySelector("#checkBtn")
 
 submitBtn.addEventListener('click', submitHandler);
@@ -14,34 +14,38 @@ function submitHandler(){
     
     console.log(ip);
     if(initialPrice.value === "" || qunatity.value === "" || currentPrice.value === ""){
-        errText.textContent = "Please enter all the values";
+        outputText.textContent = "Please enter all the values";
         return
     }
     if(ip <= 0 || qty <= 0 || curr <= 0){
-        errText.textContent = "Only values greater than 0 are allowed";
-        
+        outputText.textContent = ""
+        outputText.textContent = "Only values greater than 0 are allowed";
+        return
     }
     else{
-        errText.textContent = ""
+        outputText.textContent = ""
         calculateProfitAndLoss(ip, qty, curr);
     }
 
 }
 
-function calculateProfitAndLoss(initialPrice, qunatity, currentPrice){
+function calculateProfitAndLoss(initialPrice, quantity, currentPrice){
     if(initialPrice > currentPrice){
         // loss
-        let loss = (initialPrice - currentPrice) * qunatity
-        let lossPercentage = (loss / initialPrice) * 100;
+        let loss = (initialPrice - currentPrice) * quantity
+        let lossPercentage = (loss * 100)/ (initialPrice * quantity);
         outputText.textContent = `loss is of ₹${loss} and loss percentage is ${lossPercentage.toFixed(2)}%`;
+        outputText.style.color = "#ff0000"
     }
     else if(currentPrice > initialPrice){
         //profit
-        let profit = (currentPrice - initialPrice) * qunatity
-        let profitPercentage = (profit / initialPrice) * 100
+        let profit = (currentPrice - initialPrice) * quantity
+        let profitPercentage = (profit * 100) / (initialPrice * quantity)
         outputText.textContent = `you have a profit of ₹${profit} and profit percentage is ${profitPercentage.toFixed(2)}%`;
+        outputText.style.color = "#33ff00"
     }
     else{
         outputText.textContent =  `no profit/ no loss`;
+        outputText.style.color = "#ff7700"
     }
 }
